323871850f989a4a285e9b329b2488141a8578d9
glipitch <124493357+glipitch@users.noreply.github.com>
2025-08-30 14:52:16 +0100

    Remove NET5_0_OR_GREATER condition

--- src/Wpf.Ui.Tray/RoutedNotifyIconEvent.cs
+++ src/Wpf.Ui.Tray/RoutedNotifyIconEvent.cs
@@ -13,8 +13,5 @@ namespace Wpf.Ui.Tray;
 /// </summary>
 /// <param name="sender">Source of the event, which should be the current navigation instance.</param>
 /// <param name="e">Event data containing information about the navigation event.</param>
-#if NET5_0_OR_GREATER
-public delegate void RoutedNotifyIconEvent([NotNull] NotifyIcon sender, RoutedEventArgs e);
-#else
+
 public delegate void RoutedNotifyIconEvent(NotifyIcon sender, RoutedEventArgs e);
-#endif
