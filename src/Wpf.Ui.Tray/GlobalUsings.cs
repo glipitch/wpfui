247e57d64fea6eb5f0f32fa85cbce297b123bb4e
glipitch <124493357+glipitch@users.noreply.github.com>
2025-08-30 14:44:43 +0100

    Cleaner

--- src/Wpf.Ui.Tray/GlobalUsings.cs
+++ src/Wpf.Ui.Tray/GlobalUsings.cs
@@ -7,6 +7,5 @@ global using System;
 global using System.Collections.Generic;
 global using System.ComponentModel;
 global using System.Diagnostics;
-global using System.Diagnostics.CodeAnalysis;
 global using System.Runtime.InteropServices;
 global using System.Windows.Interop;
